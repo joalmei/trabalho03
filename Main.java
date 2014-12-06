80c3d0bffb3d37a3790edce89720b9de7a59a983
czaghi <czoliv@hotmail.com>
2014-12-06 01:07:12 -0200

    Implementacao da main

--- Main.java
+++ Main.java
@@ -92,68 +92,380 @@ public class Main {
 	}
 
 	public static void createChar (Game game)
-	{
+	{	
+
+		String name, type;
+		int special;
 		
+		System.out.print( "Chars's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while(name.equals(""));
+
+		do
+		{
+			System.out.print( "Char's type: " );
+			
+			do
+			{
+				type = s.nextLine();
+			}while(name.equals(""));
+
+			type = type.toLowerCase();
+			if (type.equals("knight"))
+			{
+				System.out.print( "Knight's power: " );
+				special = s.nextInt();
+
+				game.addChar(new Knight(name, special));
+			}
+			else if (type.equals("wizard"))
+			{
+				System.out.print( "Wizard's wisdom: " );
+				special = s.nextInt();
+
+				game.addChar(new Wizard(name, special));
+			}
+			else if (type.equals("thief"))
+			{
+				System.out.print( "Thief's stealth: " );
+				special = s.nextInt();
+
+				game.addChar(new Thief(name, special));
+			}
+			else
+			{
+				System.out.print( "NOT VALID! TRY: Knight, Thief or Wizard!\n" );
+				type = "NO";
+			}
+		}while(type.equals("NO"));
 	}
 
 	public static void createItem (Game game)
 	{
+		String name, type;
+		double price;
+		System.out.print( "Item's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while (name.equals(""));
 
+		System.out.print( "Item's price: " );
+		price = s.nextDouble();
+
+
+		System.out.print( "Item's type: " );
+		
+		do
+		{
+			type = s.nextLine();
+		}while (type.equals(""));
+
+		type = type.toLowerCase();
+
+		if (type.equals("weapon"))
+		{
+			int attackpts;
+			int range;
+
+			System.out.print( "Weapon's Attack Points: " );
+			attackpts = s.nextInt();
+
+			System.out.print( "Weapon's Range: " );
+			range = s.nextInt();
+
+			game.addItem(new Weapon (name, price, attackpts, range));
+		}
+		else if (type.equals("armor"))
+		{
+			int defensepts;
+			double weight;
+
+			System.out.print( "Armor's Defense Points: " );
+			defensepts = s.nextInt();
+
+			System.out.print( "Armor's Weight: " );
+			weight = s.nextInt();
+
+			game.addItem(new Armor (name, price, defensepts, weight));
+		}
+		else if (type.equals("potion"))
+		{
+			String ptype;
+			int restorepts;
+
+			System.out.print( "Restore Points: " );
+			restorepts = s.nextInt();
+
+			System.out.print( "Potion Type: " );
+			
+			do
+			{
+				ptype = s.nextLine();
+			}while (ptype.equals(""));
+
+
+			ptype = ptype.toLowerCase();
+			if (ptype.equals("mana"))
+				game.addItem(new ManaPotion (name, price, restorepts));
+			else if (ptype.equals("health"))
+				game.addItem(new HealthPotion (name, price, restorepts));
+			else
+				System.out.print( "NOT VALID!\n" );
+		}
+		else
+			System.out.print( "NOT VALID!\n" );
 	}
 
 	public static void createTeam (Game game)
 	{
+		String name;
+		String color;
+		Color cl = Color.black;
 
+		System.out.print( "Team's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while (name.equals(""));
+
+		do
+		{
+			System.out.print( "Team's color: " );
+			
+			do
+			{
+				color = s.nextLine();
+			}while (color.equals(""));
+
+			color = color.toLowerCase();
+			if (color.equals("blue"))
+				cl = Color.blue;
+			else if (color.equals("red"))
+				cl = Color.red;
+			else if (color.equals("green"))
+				cl = Color.green;
+			else if (color.equals("yellow"))
+				cl = Color.yellow;
+			else if (color.equals("white"))
+				cl = Color.white;
+			else if (color.equals("black"))
+				cl = Color.black;
+			else
+			{
+				System.out.print( "INVALID COLOR, TRY ONE OF THESE: blue, red, green, yellow, white, black\n" );
+				color = "invalid";
+			}
+		}while (color.equals("invalid"));
+
+		game.addTeam(new Team(name, cl));
 	}
 
 	public static void charJoinTeam (Game game)
 	{
+		String teamID;
+		String charID;
 
+		System.out.print( "GameCharacter's name: " );
+		charID = s.nextLine();
+
+		System.out.print( "Team's nome: " );
+		teamID = s.nextLine();
+
+		game.joinTeam(charID, teamID);
 	}
 
 	public static void showTeams (Game game)
 	{
-
+		game.showTeams();
 	}
 
 	public static void showChars(Game game)
 	{
-
+		game.showCharacters();
 	}
 
 	public static void powerUp (Game game)
 	{
-
+		;
 	}
 
 	public static void charEquipItem (Game game)
 	{
+		String input, name;
+		System.out.print( "Print character list? " );
+		
+		do
+		{
+			input = s.nextLine();
+		}while (input.equals(""));
 
+		input = input.toLowerCase();
+		if (input.equals("yes"))
+			game.showCharacters();
+
+		System.out.print( "Char's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while(name.equals(""));
+
+		System.out.print( "Print character's inventory? " );
+		
+		do
+		{
+			input = s.nextLine();
+		}while (input.equals(""));
+
+		input = input.toLowerCase();
+		if (input.equals("yes"))
+			game.showInventory(name);
+
+
+		String it;
+
+		System.out.print( "Item's name: " );
+
+		//Evitar o FLUSH!
+		do
+		{
+			it = s.nextLine();
+		}while (it.equals(""));
+
+		game.equipItem(name, it);
 	}
 
 	public static void charUseItem (Game game)
 	{
+		String input, name;
+		System.out.print( "Print character list? " );
+		
+		do
+		{
+			input = s.nextLine();
+		}while (input.equals(""));
+
+		input = input.toLowerCase();
+		if (input.equals("yes"))
+			game.showCharacters();
+
+		System.out.print( "Char's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while(name.equals(""));
+
+		System.out.print( "Print character's inventory? " );
+		
+		do
+		{
+			input = s.nextLine();
+		}while (input.equals(""));
+
+		input = input.toLowerCase();
+		if (input.equals("yes"))
+			game.showInventory(name);
+
+
+		String it;
+
+		System.out.print( "Item's name: " );
+
+		//Evitar o FLUSH!
+		do
+		{
+			it = s.nextLine();
+		}while (it.equals(""));
+
+		game.useItem(name, it);
 
 	}
 
 	public static void showItems(Game game)
 	{
-
+		game.showItems();
 	}
 	
 	public static void charWinsItem(Game game)
 	{
+		String input, name;
+		System.out.print( "Print character list? " );
+		
+		do
+		{
+			input = s.nextLine();
+		}while (input.equals(""));
 
+		input = input.toLowerCase();
+		if (input.equals("yes"))
+			game.showCharacters();
+
+		System.out.print( "Char's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while(name.equals(""));
+
+		String it;
+
+		System.out.print( "Item's name: " );
+
+		//Evitar o FLUSH!
+		do
+		{
+			it = s.nextLine();
+		}while (it.equals(""));
+
+		game.winItem(name, it);
 	}
 	
 	public static void charAttack(Game game)
-	{
+	{	
+		String name, enemy;
 
+		System.out.print( "Char's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while(name.equals(""));
+
+		System.out.print( "Enemy's name: " );
+		
+		do
+		{
+			name = s.nextLine();
+		}while(name.equals(""));
+
+		game.charAttack(name, enemy);
 	}
 	
 	public static void teamAttackTeam(Game game)
 	{
+		String t1,t2;
+		
+		System.out.print( "Team 1's name: " );
+		do
+		{
+			t1 = s.nextLine();
+		}while(t1.equals(""));
 
+
+		System.out.print( "Team 2's name: " );
+		do
+		{
+			t2 = s.nextLine();
+		}while(t2.equals(""));
+
+		game.teamBattle(t1, t2);
 	}
 	
 	public static void showOptions()
